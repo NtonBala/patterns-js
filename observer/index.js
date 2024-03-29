22d1feab3ebdb99bca5a9797ab20bf906cbfc81a
Anton Balashov <balashov.anton.1983@gmail.com>
2024-03-29 03:16:26 +0200

    [Observer] Add custom example

--- observer/index.js
+++ observer/index.js
@@ -1,3 +1,63 @@
 'use strict';
 
 // * Observer pattern
+class EventObserver {
+  constructor() {
+    this.subscribers = [];
+  }
+
+  subscribe(fn) {
+    this.subscribers.push(fn);
+  }
+
+  unsubscribe(fn) {
+    this.subscribers.filter((subscriber) => subscriber !== fn);
+  }
+
+  broadcast(data) {
+    this.subscribers.forEach((subscriber) => subscriber(data));
+  }
+}
+
+class TextField {
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+    this.__setupKeyUpObserver();
+  }
+
+  __setupKeyUpObserver() {
+    this.__keyUpObserver = new EventObserver();
+    this.keyUpBroadcaster = {
+      subscribe: (fn) => {
+        this.__keyUpObserver.subscribe(fn);
+      },
+      unsubscribe: (fn) => {
+        this.__keyUpObserver.unsubscribe(fn);
+      },
+    };
+
+    this.element.addEventListener('keyup', () => {
+      this.__keyUpObserver.broadcast(this.element.value);
+    });
+  }
+}
+
+class WordsCounter {
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+  }
+
+  setWordsCount = (text) => {
+    this.element.innerHTML = text ? text.trim().split(/\s+/).length : 0;
+  };
+
+  subscribeToCountBroadcaster(broadcaster) {
+    this.countBroadcaster = broadcaster;
+    this.countBroadcaster.subscribe(this.setWordsCount);
+  }
+}
+
+const textField = new TextField('.textField');
+
+const wordsCounter = new WordsCounter('.wordsCounter');
+wordsCounter.subscribeToCountBroadcaster(textField.keyUpBroadcaster);
--- observer/index.md
+++ observer/index.md
@@ -5,3 +5,6 @@
 A common problem during development that we face is when different parts of application have to respond to various events. E.g. the user has entered some text and we need to change some components. And all this is quite difficult to synchronize.
 
 Observer pattern can help us with this. It allows to create _one-to-many relation_ between components.
+
+> [!NOTE]
+> To run custom example open `./index.html` in the browser.
